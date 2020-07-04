d03bbc2c7fcafbb535141d385afd83ace79e2922
Vincent Nyanga <vince.nyanga@hotmail.com>
2020-07-04 08:51:08 +0200

    added minHeadScale model

---
+++ src/Kairos.Net/Models/MinHeadScale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Kairos.Net.Models
+{
+    public class MinHeadScale
+    {
+        public double Value { get;}
+
+        public MinHeadScale(double value)
+        {
+            if (!IsValidValue(value))
+            {
+                throw new ArgumentException(
+                    "Invalid value: minHeadScale should be between 0.015 and 0.5",
+                    nameof(value));
+            }
+            Value = value;
+        }
+
+        private bool IsValidValue(double value)
+        {
+            return value >= 0.015 && value <= 0.5;
+        }
+
+        public static explicit operator MinHeadScale(double value)
+        {
+            return new MinHeadScale(value);
+        }
+
+        public static explicit operator double(MinHeadScale minHeadScale)
+        {
+            return minHeadScale.Value;
+        }
+    }
+}
---
+++ tests/Kairos.Net.UnitTests/MinHeadScaleTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Kairos.Net.Models;
+using NUnit.Framework;
+
+namespace Kairos.Net.UnitTests
+{
+    [TestFixture]
+    public class MinHeadScalesTests
+    {
+        [TestCase(0.014)]
+        [TestCase(0.51)]
+        public void Constructor_WithInvalidValue_ThrowsException(double value)
+        {
+            Action action = () => new MinHeadScale(value);
+
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void Convert_FromDouble_ReturnsMinHeadScaleInstance()
+        {
+            var value = 0.5;
+
+            var minHeadScale = (MinHeadScale)value;
+
+            minHeadScale.Value.Should().Be(value);
+        }
+
+        [Test]
+        public void Convert_ToDouble_ReturnsCorrectValue()
+        {
+            var value = 0.5;
+            var minHeadScale = new MinHeadScale(value);
+
+            var result = (double)minHeadScale;
+
+            result.Should().Be(value);
+        }
+    }
+}
